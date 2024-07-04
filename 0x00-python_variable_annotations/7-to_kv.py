1a5424d7f9ed1285e2cf3dd0b0c15d9d836bca9a
annmulwa <annmulwa2000@gmail.com>
2024-07-04 11:21:07 +0000

    mypy

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+'''
+function to_kv that takes a string k and an
+int OR float v as arguments and returns a tuple
+'''
+from typing import Union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    '''
+    takes a string k and an int OR float
+    v as arguments and returns a tuple
+    '''
+    return (k, float(v**2))
