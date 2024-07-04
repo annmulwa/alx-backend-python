75e1b459647559ddb0cd90d52b15d740075d2840
annmulwa <annmulwa2000@gmail.com>
2024-07-04 10:34:32 +0000

    mypy

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+'''
+takes a float n as argument and returns
+the string representation of the float
+'''
+
+
+def to_str(n: float) -> str:
+    '''
+    takes a float n as argument and returns
+    the string representation of the float
+    '''
+    return str(n)
