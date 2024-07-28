e566523ffbe2c3aeb388f79d305e09883a0136c9
annmulwa <annmulwa2000@gmail.com>
2024-07-28 15:35:35 +0300

    unittests

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""
+Test the client.py file
+"""
+import requests
+import unittest
+from unittest.mock import patch, Mock, PropertyMock, call
+from parameterized import parameterized, parameterized_class
+import utils
+from utils import access_nested_map, get_json, memoize
+from client import GithubOrgClient
+import client
+from fixtures import TEST_PAYLOAD
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """
+    Implement the test_org method
+    """
+
+    @parameterized.expand([
+        ("google", {"google": True}),
+        ("abc", {"abc": True})
+    ])
+    @patch('client.get_json')
+    def test_org(self, org, expected, get_patch):
+        """
+        Test that GithubOrgClient.org returns the correct value
+        """
+        get_patch.return_value = expected
+        x = GithubOrgClient(org)
+        self.assertEqual(x.org, expected)
+        get_patch.assert_called_once_with("https://api.github.com/orgs/"+org)
