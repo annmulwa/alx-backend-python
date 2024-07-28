278f9bc727ecbcd3447dec0e3489680272579f12
annmulwa <annmulwa2000@gmail.com>
2024-07-28 14:13:24 +0300

    unittests

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -38,22 +38,20 @@ class TestAccessNestedMap(TestCase):
 
 
 class TestGetJson(TestCase):
-    """
-    Class for testing get_json function
-    """
+    """ Class for testing get_json function """
+    # order of args: test_url, test_payload
     @parameterized.expand([
         ("http://example.com", {"payload": True}),
         ("http://holberton.io", {"payload": False})
     ])
     def test_get_json(self, test_url, test_payload):
-        """
-        Test that utils.get_json returns the expected result
-        """
+        """ Test method returns correct output """
+        # set mock response to have return value of test payload
         mock_response = Mock()
         mock_response.json.return_value = test_payload
-
+        # function calls requests.get, need patch to get mock return value
         with patch('requests.get', return_value=mock_response):
             real_response = get_json(test_url)
-            self.asserEqual(real_response, test_payload)
-
+            self.assertEqual(real_response, test_payload)
+            # check that mocked method called once per input
             mock_response.json.assert_called_once()
