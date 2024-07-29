d3dacf8768d440e917d965a1d6425a52fbdc327b
annmulwa <annmulwa2000@gmail.com>
2024-07-29 15:52:24 +0300

    unittests

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -36,9 +36,33 @@ class TestGithubOrgClient(unittest.TestCase):
         """
         Unit-test GithubOrgClient._public_repos_url
         """
-        expected = "www.yes.com"
+        expected = "www.alx.com"
         payload = {"repos_url": expected}
         to_mock = 'client.GithubOrgClient.org'
         with patch(to_mock, PropertyMock(return_value=payload)):
             cli = GithubOrgClient("x")
             self.assertEqual(cli._public_repos_url, expected)
+
+    @patch('client.get_json')
+    def test_public_repos(self, get_json_mock):
+        """
+        Unit-test GithubOrgClient.public_repos
+        """
+        geofrey = {"name": "Geofrey", "license": {"key": "a"}}
+        bob = {"name": "Bob", "license": {"key": "b"}}
+        anne = {"name": "Anne"}
+        to_mock = 'client.GithubOrgClient._public_repos_url'
+        get_json_mock.return_value = [geofrey, bob, anne]
+        with patch(to_mock, PropertyMock(return_value="www.alx.com")) as y:
+            x = GithubOrgClient("x")
+            self.assertEqual(x.public_repos(), ['Geofrey', 'Bob', 'Anne'])
+            self.assertEqual(x.public_repos("a"), ['Geofrey'])
+            self.assertEqual(x.public_repos("c"), [])
+            self.assertEqual(x.public_repos(45), [])
+            get_json_mock.assert_called_once_with("www.alx.com")
+            y.assert_called_once_with()
+
+    @parameterized.expand([
+        ({'license': {'key': 'my_license'}}, 'my_license', True),
+        ({'license': {'key': 'other_license'}}, 'my_license', False)
+    ])
